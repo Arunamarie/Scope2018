1e329b91cc7b705e7e49cd7c791105f38b7841f3
uscitp-aarunasa <ShoshanaPetunia@gmail.com>
2018-11-05 02:33:57 -0800

    webhook working, ready to be deployed and subscribed - steps 6&7

---
+++ main.js
@@ -0,0 +1,8 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu } = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+// TODO: PART 3 - B
